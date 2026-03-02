2428321bbb40be0bc970f9f280b5ea133b5cbc4a
Isaque Keller <kellerisaque@gmail.com>
2026-03-02 10:10:53 -0300

    Incompleto

---
+++ ex1aeds3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void changePositions (int* vector , int position , int size , int quantity) {
+    
+}
+
+int main () {
+int* vector;
+int size;
+int quantity;
+int position;
+
+    cout << "Inform the size of the vector: " << endl;
+    cin >> size;
+
+vector = new int [size];
+
+for (int i = 0; i < size; i++) {
+    cout << "Type the position you want to insert your information: " << endl;
+    cin >> position;    
+
+    if (vector[position] = -1) {
+    cout << "Type in your information: " << endl;
+    cin >> vector[position];
+    }
+    else {
+    
+    }
+    
+}
+}
+
+
